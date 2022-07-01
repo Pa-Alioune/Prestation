c9370c8e0515242fd70f6f9aaf5a8b40df2a472d
Abdoulaye Ndiaye <elhadjiabdoulayen69@gmail.com>
2022-07-01 23:35:41 +0000

    master

---
+++ Middlewares/admin.js
@@ -0,0 +1,9 @@
+module.exports = (request,response,next)=>{
+ 
+    if (request.session.user !== undefined && request.session.user.role == "admin" ) {
+        next()
+    }
+    else{
+        response.redirect("/login")
+    }
+}
